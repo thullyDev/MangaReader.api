d2b81d42c7dbea1e56bf4f370143f90845bd7eab
thullyDev <thully.developer@gmail.com>
2024-04-07 21:05:19 +0200

    added filter in the code

--- app/routers/manga_reader_router/manga_reader.py
+++ app/routers/manga_reader_router/manga_reader.py
@@ -1,10 +1,8 @@
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Optional, Union
 from bs4 import BeautifulSoup
 from app.handlers.api_handler import ApiHandler
 from app.resources.errors import CRASH
-from .blueprints import (
-        features_blueprint,
-    )
+from pprint import pprint
 
 api = ApiHandler("https://mangareader.to")
 
@@ -14,9 +12,10 @@ async def get_featured_mangas() -> Union[List[Dict[str, str]], int]:
     if type(response) is int:
         return CRASH
 
-    soup: BeautifulSoup = BeautifulSoup(response, 'html.parser')
+    soup = get_soup(html=response)
     items: List = soup.select('.deslide-item')
     data: List[Dict[str, str]] = []
+
     for item in items:
         slug = item.select('.deslide-cover')[0].get('href')
         description = item.select('.sc-detail > .scd-item.mb-3')[0].text
@@ -32,3 +31,45 @@ async def get_featured_mangas() -> Union[List[Dict[str, str]], int]:
         })
 
     return data
+
+async def get_filter_mangas(*, params={}) -> Union[List[Dict[str, str]], int]:
+    filter_data: Dict[str, Optional[str]] = {
+        key: value
+        for key, value in params.items()
+        if value and key in { "type", "score", "rating_type", "sort", "genres", "language" }
+    }
+
+    response: Any = await api.get(endpoint="/filter", params=filter_data, html=True)
+    
+    if type(response) is int:
+        return CRASH
+
+    soup = get_soup(html=response)
+    items: List = soup.select('.item.item-spc')
+    data: List[Dict[str, str]] = []
+    
+    for item in items:
+        genres = item.select(".fdi-item.fdi-cate>a")
+        chapters = item.select(".chapter>a")[0].get("href").split("-")[-1]
+        slug = item.select('.manga-poster')[0].get('href')
+        image_data: Dict[str, str] = get_data_from_image(item)
+        data.append({
+            "chapters": chapters,
+            "slug": slug,
+            **image_data
+        })
+
+    return data
+
+def get_soup(html) -> BeautifulSoup:
+    return BeautifulSoup(html, 'html.parser')
+
+def get_data_from_image(item):
+    image = item.select('.manga-poster-img')[0]
+    image_url = image.get('src')
+    title = image.get('alt')
+
+    return {
+        "title": title,
+        "image_url": image_url,
+    }
--- app/routers/manga_reader_router/manga_reader_router.py
+++ app/routers/manga_reader_router/manga_reader_router.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, params
 from fastapi.responses import JSONResponse
 from ...handlers import ResponseHandler
-from typing import Any, Dict, List, Union
-from .manga_reader import get_featured_mangas
+from typing import Any, Dict, List, Union, Optional
+from .manga_reader import (
+     get_featured_mangas,
+     get_filter_mangas
+)
 
 router: APIRouter = APIRouter(prefix="/manga")
 response: ResponseHandler = ResponseHandler()
@@ -10,5 +13,25 @@ response: ResponseHandler = ResponseHandler()
 @router.get("/features")
 async def features() -> JSONResponse:
      data: Union[List[Dict[str, str]], int] = await get_featured_mangas()
+     return response.successful_response({"data": data })
+
+@router.get("/filter")
+async def filter(
+     language: Optional[str] = None, 
+     genres: Optional[str] = None, 
+     sort: Optional[str] = None, 
+     status: Optional[str] = None, 
+     read_type: Optional[str] = None, 
+     rating_type: Optional[str] = None
+     ) -> JSONResponse:
+     data: Union[List[Dict[str, str]], int] = await get_filter_mangas(params={
+          "language": language,
+          "genres": genres,
+          "rating_type": rating_type,
+          "sort": sort,
+          "status": status,
+          "type": read_type,
+
+     })
      return response.successful_response({"data": data })
 
